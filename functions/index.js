cecdcc0ca8a26ee204a257cf89568f15571ac56e
aubairakifSys <aubair.akif@systems.com>
2023-05-28 01:14:25 +0500

    Function setup

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,2 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const cors = require("cors");
-
-const stripe = require("stripe")(
-  "sk_test_51NBlAwHgnzHIdfZgDGJ1EBJ29QPZvJoWZEB9rAqKY7WU76WcpopasteNpPwXzYcFVkzWTkDa56iTfdl8XsXswPW700XJO25hWK"
-);
-
-const app = express();
-app.use(cors({ origin: true }));
-app.use(express.json());
-
-app.get("/", (request, response) => response.status(200).send("hello world"));
-app.post("/payments/create", async (request, response) => {
-  const total = request.query.total;
-  console.log("Payment Request Received for this amount :", total);
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total, // subunits of the currency
-    currency: "usd",
-  });
-  // OK! Created
-  response.status(201).send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
-
-exports.api = functions.https.onRequest(app);
+const { initializeApp } = require("firebase-admin/app");
+initializeApp();
